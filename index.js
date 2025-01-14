6105df4bc943463df902cd8e909bb7fd4c1de3e9
nikhil.pandit.901@gmail.com <nikhil.pandit.901@gmail.com>
2025-01-14 08:58:58 +0530

    Revived

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const btn=document.querySelector('button');
 const img=document.querySelector('img');
 
+
 btn.addEventListener('click',()=>{
-const xhr=new XMLHttpRequest();
+    const xhr=new XMLHttpRequest();
+
 xhr.responseType='json';
 xhr.addEventListener('load',()=>{
-    console.log(xhr.response.message);
     img.src=xhr.response.message;
 })
-xhr.open('GET','https://dog.ceo/api/breeds/image/random');
-xhr.send();
+
+
+    xhr.open('GET','https://dog.ceo/api/breeds/image/random');
+    xhr.send();
 })
